a7e7137ab8c2e2586d8f30f48c2466ff2eb103b7
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-02 16:41:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/tags/1.1@52815 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mp-admin/includes/install.php
@@ -0,0 +1,79 @@
+<?php
+		global $wpdb;
+
+		$charset_collate = '';
+		if ( $wpdb->supports_collation() ) 
+		{
+			if ( ! empty($wpdb->charset) ) $charset_collate  = "DEFAULT CHARACTER SET $wpdb->charset";
+			if ( ! empty($wpdb->collate) ) $charset_collate .= " COLLATE $wpdb->collate";
+		}
+
+	  	if (!MailPress::tableExists($wpdb->mp_subscribers)) 
+		{
+			add_option ('MailPress_monthly', date('Ym'));
+			add_option ('MailPress_template', 'default');
+			add_option ('MailPress_stylesheet', 'default');
+			add_option ('MailPress_current_theme', 'MailPress Default');
+
+	  		$sql = "CREATE TABLE $wpdb->mp_users (
+										id 				bigint(20) 				UNSIGNED NOT NULL AUTO_INCREMENT, 
+										email 			varchar(100) 			NOT NULL, 
+										status 			enum('waiting', 'active')	NOT NULL, 
+										confkey 			varchar(100)			NOT NULL, 
+										created			timestamp 				NOT NULL default '0000-00-00 00:00:00',
+										created_IP 			varchar(100) 			NOT NULL default '',
+										created_agent		varchar(255) 			NOT NULL default '',
+										created_user_id 		bigint(20) 				UNSIGNED NOT NULL default 0,
+										created_country		char(2)				NOT NULL default 'ZZ',
+										created_US_state		char(2)				NOT NULL default 'ZZ',
+										laststatus 			timestamp 				NOT NULL default '0000-00-00 00:00:00',
+										laststatus_IP		varchar(100) 			NOT NULL default '',
+										laststatus_agent 		varchar(255) 			NOT NULL default '',
+										laststatus_user_id	bigint(20) 				UNSIGNED NOT NULL default 0,
+										UNIQUE KEY id (id)
+									    ) $charset_collate;";
+
+			require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+	  		dbDelta($sql);
+	  	}
+	  	if (!MailPress::tableExists($wpdb->mp_stats)) 
+		{
+	  		$sql = "CREATE TABLE $wpdb->mp_stats (
+									  	sdate 			date 					NOT NULL,
+										stype 			char(1) 				NOT NULL,
+										slib 				varchar(45) 			NOT NULL,
+										scount 			bigint 				NOT NULL,
+										PRIMARY KEY(sdate, stype, slib)
+									    ) $charset_collate;";
+
+			require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+	  		dbDelta($sql);
+	  	}
+	  	if (!MailPress::tableExists($wpdb->mp_mails)) 
+		{
+	  		$sql = "CREATE TABLE $wpdb->mp_mails (
+										id 				bigint(20) 				UNSIGNED NOT NULL AUTO_INCREMENT, 
+										status 			enum('draft', 'sent','')	NOT NULL, 
+										theme				varchar(255) 			NOT NULL default '',
+										themedir			varchar(255) 			NOT NULL default '',
+										template			varchar(255) 			NOT NULL default '',
+										fromemail	 		varchar(255) 			NOT NULL default '',
+										fromname	 		varchar(255) 			NOT NULL default '',
+										toname	 		varchar(255) 			NOT NULL default '',
+										charset	 		varchar(255) 			NOT NULL default '',
+										subject			varchar(255) 			NOT NULL default '',
+										created			timestamp 				NOT NULL default '0000-00-00 00:00:00',
+										created_user_id 		bigint(20) 				UNSIGNED NOT NULL default 0,
+										sent				timestamp 				NOT NULL default '0000-00-00 00:00:00',
+										sent_user_id 		bigint(20) 				UNSIGNED NOT NULL default 0,
+										toemail	 		longtext				NOT NULL,
+									  	plaintext			longtext 				NOT NULL,
+									  	html 				longtext 				NOT NULL,
+
+										UNIQUE KEY id (id)
+									    ) $charset_collate;";
+
+			require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
+	  		dbDelta($sql);
+	  	}
+?>
---
+++ mp-content/themes/plaintext/test.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: test
+*/
+?>
+<?php $this->get_header() ?>
+
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed nisi pede, tincidunt non, vehicula ac, 
+auctor pulvinar, augue. Donec diam neque, hendrerit rhoncus, pellentesque non, ultrices quis, lacus. 
+Nam quis leo nec magna rutrum vehicula. Aenean volutpat. Duis pharetra purus mattis arcu. 
+Donec interdum orci eget sem vestibulum consectetuer. Maecenas bibendum erat id libero. Morbi congue. 
+Donec sodales interdum nulla. Curabitur eu velit et orci euismod convallis. In lobortis posuere nisi. 
+Pellentesque nec libero eu ligula accumsan adipiscing. Aenean vel mauris. Aliquam rutrum turpis nec augue. 
+Duis massa magna, faucibus sed, lobortis quis, suscipit quis, eros. Curabitur urna. 
+Suspendisse iaculis nibh sed sem. 
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+<?php $this->get_footer() ?>
