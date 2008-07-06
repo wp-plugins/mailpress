92d7c40823d05edc2e1c9d55e2d8de99ce4aefe9
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-06 22:14:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@53458 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ xtras/pt_MailPress.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: MailPress
+*/
+?>
+
+<?php include(MP_PATH . 'mp-includes/mp-manage-user.php'); ?>
+
+<?php $results = mp_manage_subscriber(); ?>
+
+<?php get_header(); ?>
+
+	<div id='content' class='narrowcolumn'>
+		<div class='post' id='post-MailPress'>
+		<h2><?php echo $results ['title']; ?></h2>
+			<div class='entry'>
+				<?php echo $results ['content']; ?>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
