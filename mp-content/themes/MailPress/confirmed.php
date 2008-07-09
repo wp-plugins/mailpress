7905e2432333ef5be6a5fdc1612411f0e6fbc8b9
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-09 14:36:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/tags/1.2@53959 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mp-content/themes/MailPress/confirmed.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: confirmed
+Subject: [<?php bloginfo('name');?>] <?php printf('%s confirmed', $this->args->toemail); ?>
+*/
+?>
+<?php $this->get_header() ?>
+			<table style='margin:0;padding:0;border:none;width:100%;'>
+				<tr>
+					<td style='margin:0;padding:0pt 0pt 20px 45px;border:none;width:450px;float:left;color:#333333;text-align:left;font-family:Verdana,Sans-Serif;'>
+						<div style='margin:0pt 0pt 40px;padding:0;border:none;text-align:justify;'>
+							<h2 style='margin:30px 0pt 0pt;padding:0;border:none;color:#333;font-size:1.4em;font-weight:bold;font-family:Verdana,Sans-Serif;'>
+Congratulations !
+							</h2>
+							<small style='margin:0;padding:0;border:none;line-height:2em;color:#777;font-size:0.7em;font-family:Arial,Sans-Serif;'>
+<?php echo mysql2date('F j, Y', current_time('mysql')); ?>
+							</small>
+							<div style='margin:0;padding:0;border:none;'>
+								<p style='margin:0;padding:0;border:none;line-height:1.4em;font-size:0.85em;'>
+<?php printf( 'You are now a subscriber of : %s', "<a style='color:#000;' href='" . get_option('siteurl') . "'>" . get_option('blogname') . "</a>" ); ?>
+								</p>
+							</div>
+						</div>
+					</td>
+				</tr>
+			</table>
+<?php $this->get_footer() ?>
