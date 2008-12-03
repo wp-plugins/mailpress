2a6d81d97be2a6f1ed480bee22aacfdca9e2d91c
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-03 15:34:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@77272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp-admin/mails.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-if (!current_user_can('MailPress_edit_mails')) wp_die(__('You do not have sufficient permissions to access this page.'));
-
-if ( isset($_GET['file']) && ($_GET['file'] == 'mail')) 	include('includes/mail.php');
-else										include('includes/mails.php');
-?>
