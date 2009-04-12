f5bc46aa6932af703de874db57681a52b492aee3
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-12 16:48:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@110024 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp-admin/mails.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if (!current_user_can('MailPress_edit_mails')) wp_die(__('You do not have sufficient permissions to access this page.'));
-
-if ( isset($_GET['file']))
-	if ('mail' == $_GET['file']) 	include('includes/mail.php');
-	else 					include('mail_new.php');
-else						include('includes/mails.php');
-?>
--- mp-content/themes/nohtml/daily.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-Template Name: daily
-Subject: [<?php bloginfo('name');?>]  Les articles d'hier
-*/
-?>
