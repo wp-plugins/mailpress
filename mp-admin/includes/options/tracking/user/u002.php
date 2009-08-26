0547e91246ea81b61a61f3a5939f4523c2b6b9ed
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-26 00:42:47 +0000

    cosmetic .... on u2
    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@149119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp-admin/includes/options/tracking/user/u002.php
+++ mp-admin/includes/options/tracking/user/u002.php
@@ -16,12 +16,21 @@ class MP_Tracking_module_u002 extends MP_Tracking_module_abstract
 		global $wpdb;
 		$query = "SELECT * FROM $wpdb->mp_usermeta WHERE user_id = " . $mp_user->id . " AND meta_key = '_MailPress_mail_sent' ORDER BY umeta_id DESC LIMIT 10;";
 		$tracks = $wpdb->get_results($query);
-		if ($tracks) foreach($tracks as $track)
+		if ($tracks)
 		{
-			$subject = $wpdb->get_var("SELECT subject FROM $wpdb->mp_mails WHERE id = " . $track->meta_value . ';');
-			$subject = ($subject) ? $subject : __('(deleted)','MailPress');
-			echo '(' . $track->meta_value . ') ' . $subject . '<br />';
-		} 
+			MailPress::require_class('Mailmeta');
+			foreach($tracks as $track)
+			{
+				$subject = $wpdb->get_var("SELECT subject FROM $wpdb->mp_mails WHERE id = " . $track->meta_value . ';');
+				if ($subject)
+				{
+					$metas = MP_Mailmeta::get( $track->meta_value, '_MailPress_replacements');
+					if ($metas) foreach($metas as $k => $v) $subject = str_replace($k, $v, $subject);
+				}
+				else $subject = __('(deleted)','MailPress');
+				echo '(' . $track->meta_value . ') ' . $subject . '<br />';
+			} 
+		}
 	}
 }
 $MP_Tracking_module_u002 = new MP_Tracking_module_u002();
