0ae25fd85218c12b8a2a44ee67d30ea7e09f9f3f
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-17 10:48:38 +0000

    new_user MailPress theme
    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@155677 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp-content/themes/MailPress/new_user.php
+++ mp-content/themes/MailPress/new_user.php
@@ -4,32 +4,35 @@ Template Name: new_user
 */
 ?>
 <?php $this->get_header() ?>
-			<table <?php mp_classes('nopmb ctable'); ?>>
-				<tr>
-					<td <?php mp_classes('nopmb ctd'); ?>>
-						<div <?php mp_classes('cdiv'); ?>>
-							<h2 <?php mp_classes('ch2'); ?>>
-							</h2>
-							<small <?php mp_classes('nopmb cdate'); ?>>
+
+<table <?php $this->classes('nopmb ctable'); ?>>
+	<tr>
+		<td <?php $this->classes('nopmb ctd'); ?>>
+			<div <?php $this->classes('cdiv'); ?>>
+				<h2 <?php $this->classes('ch2'); ?>>
+Congratulations !
+				</h2>
+				<small <?php $this->classes('nopmb cdate'); ?>>
 <?php echo mysql2date('F j, Y', current_time('mysql')); ?>
-							</small>
-							<div <?php mp_classes('nopmb'); ?>>
-								<p <?php mp_classes('nopmb cp'); ?>>
+				</small>
+				<div <?php $this->classes('nopmb'); ?>>
+					<p <?php $this->classes('nopmb cp'); ?>>
 <?php printf(__('Username: %s'), $this->args->u->login ); ?>
 									<br/>
 <?php 
 if (isset($this->args->admin))	printf(__('E-mail: %s'),   $this->args->u->email );
 else 						printf(__('Password: %s'), $this->args->u->pwd );
 ?>
-									<br/><br/>
-									<a <?php mp_classes('clink2'); ?> href='<?php echo get_option('siteurl'); ?>/wp-login.php'>
-										<?php _e('Log in'); ?>
-									</a>.
-									<br/><br/>
-								</p>
-							</div>
-						</div>
-					</td>
-				</tr>
-			</table>
+						<br/><br/>
+						<a <?php $this->classes('clink2'); ?> href='<?php echo get_option('siteurl'); ?>/wp-login.php'>
+							<?php _e('Log in'); ?>
+						</a>.
+						<br/><br/>
+					</p>
+				</div>
+			</div>
+		</td>
+	</tr>
+</table>
+
 <?php $this->get_footer() ?>
