b6596d42a20859cae9adb906b0dd30f7e15b3bf6
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-16 12:10:42 +0000

    install
    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@174141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp-admin/includes/install/form.php
+++ mp-admin/includes/install/form.php
@@ -13,12 +13,11 @@
 		}
 
   		$sql = "CREATE TABLE $wpdb->mp_forms (
-									id 				bigint(20) 				NOT NULL auto_increment,
-									label	 			varchar(255) 			NOT NULL default '',
-									description			varchar(255) 			NOT NULL default '',
-									template			varchar(50) 			NOT NULL default '',
-									settings			longtext,
-
+									id  				bigint(20) 				NOT NULL auto_increment,
+									label  			varchar(255) 			NOT NULL default '',
+									description 		varchar(255) 			NOT NULL default '',
+									template 			varchar(50) 			NOT NULL default '',
+									settings 			longtext,
 									UNIQUE KEY id (id)
 								    ) $charset_collate;";
 
@@ -26,15 +25,14 @@
 
   		dbDelta($sql);
   		$sql = "CREATE TABLE $wpdb->mp_fields (
-									id 				bigint(20) 				NOT NULL auto_increment,
-									form_id 			bigint(20) 				NOT NULL,
-									ordre		 		bigint(20) 				UNSIGNED NOT NULL default 0,
-									type	 			varchar(50) 			NOT NULL default '',
-									template			varchar(50) 			NOT NULL default '',
-									label	 			varchar(255) 			NOT NULL default '',
-									description			varchar(255) 			NOT NULL default '',
-									settings			longtext,
-
+									id  				bigint(20) 				NOT NULL auto_increment,
+									form_id  			bigint(20) 				NOT NULL,
+									ordre 	 		bigint(20) 				UNSIGNED NOT NULL default 0,
+									type 	 			varchar(50) 			NOT NULL default '',
+									template 			varchar(50) 			NOT NULL default '',
+									label  			varchar(255) 			NOT NULL default '',
+									description 		varchar(255) 			NOT NULL default '',
+									settings 			longtext,
 									UNIQUE KEY id (id)
 								    ) $charset_collate;";
 
--- mp-admin/includes/install/tracking.php
+++ mp-admin/includes/install/tracking.php
@@ -13,20 +13,20 @@
 		}
 
   		$sql = "CREATE TABLE $wpdb->mp_tracks (
-									id 				bigint(20) 				UNSIGNED NOT NULL AUTO_INCREMENT, 
-									user_id 			bigint(20) 				NOT NULL default '0',
-									mail_id 			bigint(20) 				NOT NULL default '0',
-									tmstp				timestamp 				NOT NULL default '0000-00-00 00:00:00',
-									mmeta_id 			bigint(20) 				NOT NULL default '0',
-									context	 		varchar(20) 			NOT NULL default 'html',
-									ip	 			varchar(100) 			NOT NULL default '',
-									agent				varchar(255) 			NOT NULL default '',
-									track	 			longtext,
-									referrer 			longtext,
+									id  				bigint(20) 				UNSIGNED NOT NULL AUTO_INCREMENT, 
+									user_id  			bigint(20) 				NOT NULL default '0',
+									mail_id  			bigint(20) 				NOT NULL default '0',
+									tmstp 			timestamp 				NOT NULL default '0000-00-00 00:00:00',
+									mmeta_id  			bigint(20) 				NOT NULL default '0',
+									context 	 		varchar(20) 			NOT NULL default 'html',
+									ip 	 			varchar(100) 			NOT NULL default '',
+									agent 			varchar(255) 			NOT NULL default '',
+									track  			longtext,
+									referrer  			longtext,
 									UNIQUE KEY id (id),
-									KEY user_id   (user_id),
-									KEY mail_id   (mail_id),
-									KEY mmeta_id  (mmeta_id)
+									KEY user_id  (user_id),
+									KEY mail_id  (mail_id),
+									KEY mmeta_id (mmeta_id)
 								    ) $charset_collate;";
     
 			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
