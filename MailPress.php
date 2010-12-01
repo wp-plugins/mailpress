73b5beb8417847fe0bf6fe4e1974a57fab61d7c9
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-01 00:44:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@317194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MailPress.php
+++ MailPress.php
@@ -5,6 +5,7 @@ Plugin URI: http://www.mailpress.org
 Description: The WordPress mailing platform. <b>(do not use automatic upgrade!)</b>
 Author: Andre Renaut
 Requires at least: 3.0
+Tested up to: 3.1
 Version: 5.0.1
 Author URI: http://www.mailpress.org
 */
