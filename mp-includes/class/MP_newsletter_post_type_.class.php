9b2dc86000b764db2df2f4b9c9f96b63cf1f6c6f
arena <arena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-14 12:34:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mailpress/trunk@530976 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mp-includes/class/MP_newsletter_post_type_.class.php
+++ mp-includes/class/MP_newsletter_post_type_.class.php
@@ -39,7 +39,7 @@ abstract class MP_newsletter_post_type_
 
 	function subscriptions_newsletter_th($th, $newsletter)
 	{
-		if (	isset($newsletter['params']['post_type']) 	&& $this->post_type == $newsletter['params']['post_type'])
+		if (	isset($newsletter['params']['post_type']) 	&& $this->post_type == $newsletter['params']['post_type'] && !isset($newsletter['params']['taxonomy']) )
 			return '** ' . $newsletter['mail']['the_post_type'] . ' **';
 		return $th;
 	}
